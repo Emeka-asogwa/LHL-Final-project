3ed6b32ea7ef9f31399f796e611c8c248541ace9
Kathy <kathy.tam@mail.mcgill.ca>
2021-08-28 01:35:35 +0000

    Update to use db

--- server/routes/register.js
+++ server/routes/register.js
@@ -2,40 +2,42 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
-  // const user = req.body;
-  // const templateVars = {};
+module.exports = (db) => {
+  router.post("/", (req, res) => {
+    const user = req.body;
+  
+    // // If inputs blank, error
+    // Hash password
+    user.password = bcrypt.hashSync(user.password, 12);
+    const name = `${user.firstName} ${user.lastName}`;
+    // // If email already registered, error
+    // Else store new user in database: register query
+    db.query(
+      `
+      INSERT INTO users(name, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+      `,
+      [name, user.email, user.password]
+    )
+    .then((data) => {
+      // const newUser = data.rows[0];
+      // req.session.userId = newUser.id;
+      console.log("User created successfully")
+      res.send("User created");
+    })
+    .catch((e) => {
+      res.status(500);
+      if (e.constraint === "users_email_key") {
+        res.send("Email already registered");
+      } else {
+        res.send(e);
+      }
+    });
+  });
 
-  // // If inputs blank, error
-  // // Hash password
-  // user.password = bcrypt.hashSync(user.password, 12);
+  return router;
+};
 
-  // // If email already registered, error
-  // // Else store new user in database: register query
-  // db.query(
-  //   `
-  //   INSERT INTO users(name, email, password)
-  //   VALUES ($1, $2, $3)
-  //   RETURNING *;
-  //   `,
-  //   [user.firstName + user.lastName, user.email, user.password]
-  // )
-  // .then((data) => {
-  //   const newUser = data.rows[0];
-  //   req.session.userId = newUser.id;
-  //   res.redirect("/");
-  // })
-  // .catch((e) => {
-  //   res.status(500);
-  //   if (e.constraint === "users_email_key") {
-  //     templateVars.error = "Email already registered";
-  //     res.send("Email already registered");
-  //   } else {
-  //     res.send(e);
-  //   }
-  // });
-});
 
-module.exports = router;
+// module.exports = router;
