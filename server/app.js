944b9bca817418bf37dcd758a9957cf0f8874c24
Kathy <kathy.tam@mail.mcgill.ca>
2021-08-28 01:35:17 +0000

    Pass db to router

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/register", registerRouter);
+app.use("/register", registerRouter(db));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
