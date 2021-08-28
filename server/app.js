a68cd411f47d1c37585cee4136ed10ec1d69e84b
Kathy <kathy.tam@mail.mcgill.ca>
2021-08-28 15:30:23 +0000

    Setup cookie-session

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const logger = require("morgan");
 
 // Routers
@@ -32,7 +32,12 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["encryptdemcookies"],
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
