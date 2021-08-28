b0233369910436486d63b237fd48499c015546cc
Kathy <kathy.tam@mail.mcgill.ca>
2021-08-28 15:13:18 +0000

    Change icon

--- front-end/src/Register.js
+++ front-end/src/Register.js
@@ -6,7 +6,7 @@ import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -70,7 +70,7 @@ export default function Register() {
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+          <AccountBoxIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
           Register
