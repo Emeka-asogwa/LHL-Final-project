5479bbd35bf842da672739629c4207bbc21f6c8b
Kathy <kathy.tam@mail.mcgill.ca>
2021-09-01 00:04:55 +0000

    Add some styling to Start page

--- front-end/src/Start.js
+++ front-end/src/Start.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import axios from './config/axios';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,6 +14,11 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
       width: '50ch',
     },
+  },
+  img: {
+    width: 300,
+    height: 300,
+    borderRadius: '50%',
   },
 }));
 
@@ -44,15 +50,21 @@ export default function Start(props) {
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
+      <img className={classes.img} alt="cartoon-date" src="https://media.istockphoto.com/vectors/illustration-of-romantic-date-vector-id1056840218?k=20&m=1056840218&s=612x612&w=0&h=1PX5kGppsR5BGi_rqfDvjoGfqKgW5ySRV-LNcAKX-00="/>
       <Typography component="h1" variant="h5">
-        Welcome {userName}!
+        Welcome, {userName}!
       </Typography>
-      <Typography component="h1" variant="h5">
+      <Typography component="h1" variant="subtitle1" color="textSecondary">
         You currently have no date spots.
       </Typography>
-      <Link to={{pathname:"/generatelist", state: { userId: history.location.state?.userId}}} variant="body2">
-        Click here to get started.
-      </Link>
+      <Button
+        variant="contained"
+        color="primary"
+      >
+        <Link to={{pathname:"/generatelist", state: { userId: history.location.state?.userId}}} style={{ textDecoration: 'none', color: "white" }}>
+          Start building your list
+        </Link>
+      </Button>
     </Container>
   )
 }
